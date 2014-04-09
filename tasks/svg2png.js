e8112d2776470ca128bf11f4d03d55613ad33c59
Pascal Duez <pascal@duez.me>
2014-04-09 21:29:44 +0200

    Default async limit to num of cpus.

--- tasks/svg2png.js
+++ tasks/svg2png.js
@@ -5,26 +5,28 @@ var path = require("path");
 var async = require("async");
 var svg2png = require("svg2png");
 var chalk = require("chalk");
+var numCPUs = require("os").cpus().length;
 
 module.exports = function (grunt) {
 
   grunt.registerMultiTask("svg2png", "Convert SVG to PNG", function () {
-    var options = this.options();
-    var limit = options.limit || 10;
+    var options = this.options({
+      scale: 1.0,
+      subdir: "",
+      limit: Math.max(numCPUs, 2)
+    });
 
-    async.eachLimit(this.files, limit, function (el, next) {
-      var scale = options.scale || 1.0;
-      var subdir = options.subdir || "";
+    async.eachLimit(this.files, options.limit, function (el, next) {
       var rootdir = path.dirname(el.src);
       var pngFile = path.basename(el.src, ".svg") + ".png";
-      var dest = path.join(rootdir, subdir, pngFile);
+      var dest = path.join(rootdir, options.subdir, pngFile);
 
-      svg2png(el.src, dest, scale, function (err) {
+      svg2png(el.src, dest, options.scale, function (err) {
         if (err) {
           grunt.log.error("An error occurred converting %s in %s: %s", el.src, dest, err);
         }
         else {
-          grunt.log.writeln(chalk.green("✔ ") + dest + chalk.gray(" (scale:", scale + ")"));
+          grunt.log.writeln(chalk.green("✔ ") + dest + chalk.gray(" (scale:", options.scale + ")"));
         }
         next();
       });
